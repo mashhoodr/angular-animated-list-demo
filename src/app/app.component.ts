9dcf8710b8f41f8e4121e197a4287ca369432fd8
Mashhood Rastgar <mashhoodr@gmail.com>
2018-09-21 21:38:27 +0500

    configured advanced animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { trigger, transition, query, stagger, animateChild, style, animate } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  animations: [
+    trigger('list', [
+      transition(':enter', [
+        // child animation selector + stagger
+        query('@items', 
+          stagger(300, animateChild())
+        )
+      ]),
+    ]),
+    trigger('items', [
+      // cubic-bezier for a tiny bouncing feel
+      transition(':enter', [
+        style({ transform: 'scale(0.5)', opacity: 0 }),
+        animate('1s cubic-bezier(.8,-0.6,0.2,1.5)', 
+          style({ transform: 'scale(1)', opacity: 1 }))
+      ]),
+      transition(':leave', [
+        style({ transform: 'scale(1)', opacity: 1, height: '*' }),
+        animate('1s cubic-bezier(.8,-0.6,0.2,1.5)', 
+          style({ transform: 'scale(0.5)', opacity: 0, height: '0px', margin: '0px' }))
+      ]),      
+    ])
+  ]
 })
 export class AppComponent {
 
